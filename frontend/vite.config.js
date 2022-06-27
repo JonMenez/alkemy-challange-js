f861985fffbc8127a4be2dfade8027e04caa0132
Jon_menez <joanf.gimenez@gmail.com>
2022-06-27 20:14:42 -0300

    [UPDATE] alias

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+const path = require('path');
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@styles': path.resolve(__dirname, './src/styles'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@containers': path.resolve(__dirname, './src/containers'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@images': path.resolve(__dirname, './src/assets/images'),
+      '@icons': path.resolve(__dirname, './src/assets/icons'),
+      '@fonts': path.resolve(__dirname, './src/assets/fonts'),
+    },
+  },
   plugins: [react()]
 })
