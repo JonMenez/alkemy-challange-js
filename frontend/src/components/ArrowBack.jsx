b764b50adb863a953888b759de080fb0325a1cf3
Jon_menez <joanf.gimenez@gmail.com>
2022-06-27 20:15:55 -0300

    [ADD] arrow back component

---
+++ frontend/src/components/ArrowBack.jsx
@@ -0,0 +1,12 @@
+import arrowBack from '@icons/arrow-back.svg'
+import '@styles/arrowBack.scss'
+
+const ArrowBack = () => {
+    return (
+        <div className="arrow-back">
+            <img src={arrowBack} alt="arrow back icon" />
+        </div>
+    )
+}
+
+export default ArrowBack
