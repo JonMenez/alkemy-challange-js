c2eeae76cf0a026d91f0cfa53b46ce763be16f91
Jon_menez <joanf.gimenez@gmail.com>
2022-06-28 21:44:45 -0300

    [ADD] initial page

--- frontend/src/pages/WelcomePage.jsx
+++ frontend/src/pages/WelcomePage.jsx
@@ -1,7 +1,24 @@
+import Button from "@components/Button"
+import googleSVG from "@icons/google.svg"
+import '@styles/welcome.scss'
 
 const WelcomePage = () => {
     return (
-        <div>WelcomePage</div>
+        <main className="welcome">
+            <div className="welcome-container">
+                <h1 className="welcome-logo">Moniger</h1>
+                <p className="welcome-text">Better way to manage <span>your expenses</span></p>
+                <div>
+                    <Button title="Sign Up" classButton="welcome-button-primary" />
+                    <Button title="Login" classButton="welcome-button-secondary" />
+                </div>
+                <p className="welcome-optional">or</p>
+                <button className="welcome-google">
+                    <img src={googleSVG} alt="google-logo" />
+                    <span className="welcome-google-text">Sign in with Google</span>
+                </button>
+            </div>
+        </main>
     )
 }
 
