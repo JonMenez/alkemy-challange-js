caa18efa4fb2bc055670cf740c0c69e64018a869
Jon_menez <joanf.gimenez@gmail.com>
2022-06-29 16:24:47 -0300

    [ADD] components

--- frontend/src/containers/header.jsx
+++ frontend/src/containers/header.jsx
@@ -1,13 +1,27 @@
+import { useState } from 'react'
 import NavBar from '@components/NavBar'
-import Logo from '../components/logo'
+import Logo from '@components/logo'
+import menuSVG from '@icons/menu.svg'
+import '@styles/header.scss'
 
 const Header = () => {
+    const [isOpen, setIsOpen] = useState(false)
+
+    const _handleClick = () => {
+        setIsOpen(true)
+    }
+
     return (
-        <div>
-            Header
+        <header className='header'>
+            <button className='header-menu' onClick={_handleClick}>
+                <img src={menuSVG} alt="menu icon" />
+            </button>
             <Logo />
-            <NavBar />
-        </div>
+            <NavBar
+                isOpen={isOpen}
+                setIsOpen={setIsOpen}
+            />
+        </header>
     )
 }
 
