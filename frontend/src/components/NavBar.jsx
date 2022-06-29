b70e3807602a25ef488658365abbc4208a2cb421
Jon_menez <joanf.gimenez@gmail.com>
2022-06-29 16:23:37 -0300

    [ADD] navigation fully responsive

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import Button from '@components/Button';
+import exitSVG from '@icons/exit.svg';
+import '@styles/navBar.scss'
 
-const NavBar = () => {
+const NavBar = ({ isOpen, setIsOpen }) => {
+
+    const show = isOpen ? 'show' : '';
+
+    const _handleClick = () => {
+        setIsOpen(false)
+    }
+
     return (
-        <div>NavBar</div>
+        <div className={`navBar ${show}`}>
+            <div className='navBar-logo'>
+                <h2 className="navBar-title">Moniger</h2>
+                <p className='navBar-text'>Track your activities</p>
+                <span className='navBar-underscore'></span>
+                <div onClick={_handleClick} className='navBar-exit'>
+                    <img src={exitSVG} alt="exit icon" />
+                </div>
+            </div>
+            <ul className="navBar-list">
+                <li className="navBar-list-item">
+                    <a className='navBar-list-link active' href="#">Home</a>
+                </li>
+                <li className="navBar-list-item">
+                    <a className='navBar-list-link' href="#">Profile</a>
+                </li>
+                <li className="navBar-list-item">
+                    <a className='navBar-list-link' href="#">About</a>
+                </li>
+                <li className="navBar-list-item">
+                    <a className='navBar-list-link' href="#">Contact</a>
+                </li>
+            </ul>
+            <Button
+                title='Log out'
+                classButton='navBar-button'
+            />
+        </div>
     )
 }
 
