ef0955a1183df569b8b8173f30151c237c006cf1
Jon_menez <joanf.gimenez@gmail.com>
2022-06-30 08:29:41 -0300

    [ADD] balanace components & list

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import { NavLink } from 'react-router-dom'
+import CardBalance from '@components/CardBalance'
+import Transactions from '@containers/Transactions'
+import '@styles/home.scss'
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <main className='home'>
+            <CardBalance balance={-100.432} />
+            <div className="home-container">
+                <ul className="home-list">
+                    <li className="home-list-title">Recent Transactions</li>
+                    <li className="home-list-title-link">
+                        <NavLink to='/all-transactions'>
+                            See All
+                        </NavLink>
+                    </li>
+                </ul>
+                <Transactions />
+            </div>
+        </main>
     )
 }
 
