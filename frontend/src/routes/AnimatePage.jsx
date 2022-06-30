16774c56500d857eba645e4344fa8f3eacd98f9b
Jon_menez <joanf.gimenez@gmail.com>
2022-06-30 08:27:50 -0300

    [ADD] routes Home & transactions

---
+++ frontend/src/routes/AnimatePage.jsx
@@ -0,0 +1,20 @@
+import { Routes, Route } from 'react-router-dom'
+import Layout from '@containers/Layout'
+import Home from '@pages/Home'
+import TransactionPage from '../pages/TransactionPage'
+// import Home from '@pages/index'
+
+const AnimatePage = () => {
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Home />} />
+                    <Route path="all-transactions" element={<TransactionPage />} />
+                </Route>
+            </Routes>
+        </>
+    )
+}
+
+export default AnimatePage
