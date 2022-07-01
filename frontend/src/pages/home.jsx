da5d9b3fc3990eb6b96ea4fe08d3c392d61fb4b8
Jon_menez <joanf.gimenez@gmail.com>
2022-07-01 10:32:57 -0300

    [UPDATE] path to See All link

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -2,6 +2,7 @@ import { NavLink } from 'react-router-dom'
 import CardBalance from '@components/CardBalance'
 import Transactions from '@containers/Transactions'
 import '@styles/home.scss'
+import CardTransaction from '../components/CardTransaction'
 
 const Home = () => {
     return (
@@ -11,12 +12,25 @@ const Home = () => {
                 <ul className="home-list">
                     <li className="home-list-title">Recent Transactions</li>
                     <li className="home-list-title-link">
-                        <NavLink to='/all-transactions'>
+                        <NavLink to='all-transactions/'>
                             See All
                         </NavLink>
                     </li>
                 </ul>
-                <Transactions />
+                <Transactions>
+                    <CardTransaction
+                        description={"Shoes"}
+                        balance={-100}
+                        date={'2020-01-01'}
+                        category={"Shppoing"}
+                    />
+                    <CardTransaction
+                        description={"payment"}
+                        balance={200}
+                        date={'2020-01-23'}
+                        category={"Deposit"}
+                    />
+                </Transactions>
             </div>
         </main>
     )
