5b2e28f8130b7afec62e6d58a81327c698362719
Jon_menez <joanf.gimenez@gmail.com>
2022-07-04 20:20:30 -0300

    [ADD] modal

--- frontend/src/components/CardTransaction.jsx
+++ frontend/src/components/CardTransaction.jsx
@@ -1,6 +1,9 @@
+import CardTransactionModal from '@components/CardTransactionModal'
 import '@styles/cardTransaction.scss'
+import { useState } from 'react'
 
 const CardTransaction = ({ description, balance, date, category }) => {
+    const [isOpen, setIsOpen] = useState(false)
 
     const _formatBalance = (balance) => {
         if (balance < 0) {
@@ -9,19 +12,37 @@ const CardTransaction = ({ description, balance, date, category }) => {
         return `$${balance.toFixed(2)}`
     }
 
-    const operationType = balance < 0 ? 'expense' : 'income'
+    const _handleClick = () => {
+        setIsOpen(true)
+    }
+
+    const operationType = balance < 0 ? 'Expense' : 'Income'
 
     return (
-        <div className='cardTransaction'>
-            <div className='cardTransaction-left'>
-                <p className="cardTransaction-info">{description}</p>
-                <span className='cardTransaction-span'>{date}</span>
+        <>
+            <div
+                onClick={_handleClick}
+                className='cardTransaction'>
+                <div className='cardTransaction-left'>
+                    <p className="cardTransaction-info">{description}</p>
+                    <span className='cardTransaction-span'>{date}</span>
+                </div>
+                <div className='cardTransaction-right'>
+                    <p className={`cardTransaction-number ${operationType}`}>{_formatBalance(balance)}</p>
+                    <span className='cardTransaction-span'>{category}</span>
+                </div>
             </div>
-            <div className='cardTransaction-right'>
-                <p className={`cardTransaction-number ${operationType}`}>{_formatBalance(balance)}</p>
-                <span className='cardTransaction-span'>{category}</span>
-            </div>
-        </div>
+            {
+                isOpen && <CardTransactionModal
+                    description={description}
+                    balance={_formatBalance(balance)}
+                    date={date}
+                    category={category}
+                    setIsOpen={setIsOpen}
+                    operationType={operationType}
+                />
+            }
+        </>
     )
 }
 
