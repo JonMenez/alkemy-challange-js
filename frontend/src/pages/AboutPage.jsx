0a9add2bcd41526c23f633fbd01d8f8cac553022
Jon_menez <joanf.gimenez@gmail.com>
2022-07-06 20:45:32 -0300

    [ADD] about page

---
+++ frontend/src/pages/AboutPage.jsx
@@ -0,0 +1,27 @@
+import '@styles/aboutPage.scss'
+
+const AboutPage = () => {
+    return (
+        <div className='about'>
+            <h1 className='about-title'>About Page</h1>
+            <div className="about-img">
+                <img src="#" alt="" />
+            </div>
+            <p className='about-text'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Donec euismod, nisl eget consectetur sagittis, nisl nunc
+                consectetur nisi, euismod aliquam nisl nunc euismod nisi.
+            </p>
+            <div className="about-img">
+                <img src="#" alt="" />
+            </div>
+            <p className='about-text'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Donec euismod, nisl eget consectetur sagittis, nisl nunc
+                consectetur nisi, euismod aliquam nisl nunc euismod nisi.
+            </p>
+        </div>
+    )
+}
+
+export default AboutPage
