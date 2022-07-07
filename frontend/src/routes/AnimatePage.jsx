61a94d27f4bf48de8e48e49cdb35b495b11603df
Jon_menez <joanf.gimenez@gmail.com>
2022-07-06 21:24:55 -0300

    [UPDATE] routes

--- frontend/src/routes/AnimatePage.jsx
+++ frontend/src/routes/AnimatePage.jsx
@@ -2,6 +2,8 @@ import { Routes, Route } from 'react-router-dom'
 import Layout from '@containers/Layout'
 import Home from '@pages/Home'
 import TransactionPage from '@pages/TransactionPage'
+import AboutPage from '@pages/AboutPage'
+import ContactPage from '../pages/ContactPage'
 
 const AnimatePage = () => {
     return (
@@ -10,6 +12,9 @@ const AnimatePage = () => {
                 <Route path="/" element={<Layout />}>
                     <Route index element={<Home />} />
                     <Route path="all-transactions" element={<TransactionPage />} />
+                    <Route path='transactions' element={<TransactionPage />} />
+                    <Route path='about' element={<AboutPage />} />
+                    <Route path='contact' element={<ContactPage />} />
                 </Route>
             </Routes>
         </>
