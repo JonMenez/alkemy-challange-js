938fdc248f89191df97479bfe497136b5f97e7ed
Jon_menez <joanf.gimenez@gmail.com>
2022-07-15 22:19:23 -0300

    [ADD] user model

---
+++ backend/models/user.js
@@ -0,0 +1,24 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../database/connection');
+
+const User = sequelize.define('user', {
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    status: {
+        type: DataTypes.BOOLEAN,
+
+    }
+}
+);
+
+module.exports = User;
