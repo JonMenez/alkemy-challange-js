0e9fb17962673dcf444658e3dc1671ae382daad3
Jon_menez <joanf.gimenez@gmail.com>
2022-07-19 11:56:44 -0300

    [ADD] validations & checks

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,16 +1,36 @@
 const { Router } = require('express');
+const { check } = require('express-validator');
+const { validateInputs, validJWT, validID } = require('../middlewares/');
 const { getUsers, getUserById, createUser, updateUser, deleteUser } = require('../controllers/users');
+const { existEmail } = require('../helpers/validateDB');
 
 const router = Router();
 
 router.get('/', getUsers);
 
-router.get('/:id', getUserById);
+router.get('/:id', [
+    validID,
+], getUserById);
 
-router.post('/', createUser);
+router.post('/', [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Email is not valid').isEmail(),
+    check('password', 'Password must be at least 6 characters long').isLength({ min: 6 }),
+    check('email').custom(existEmail),
+    validateInputs
+], createUser);
 
-router.put('/:id', updateUser);
+router.put('/:id', [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Email is not valid').isEmail(),
+    check('password', 'Password must be at least 6 characters long').isLength({ min: 6 }),
+    validateInputs
+], updateUser);
 
-router.delete('/:id', deleteUser);
+router.delete('/:id', [
+    validJWT,
+    validID,
+    validateInputs
+], deleteUser);
 
 module.exports = router;
