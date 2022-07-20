0fe94a5c0b91850c7addfa701a412cea3bdb63cf
Jon_menez <joanf.gimenez@gmail.com>
2022-07-20 15:58:42 -0300

    [ADD] google atuhentication route

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
-const { login } = require('../controllers/auth');
+const { login, googleSignIn } = require('../controllers/auth');
 const validateInputs = require('../middlewares/validateInputs');
 
 
@@ -11,6 +11,13 @@ router.post('/login', [
     check('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
     validateInputs
 ], login);
+
+router.post('/google', [
+    check('id_token', 'id_token required').not().isEmpty(),
+    validateInputs
+], googleSignIn);
+
+
 
 
 module.exports = router;
