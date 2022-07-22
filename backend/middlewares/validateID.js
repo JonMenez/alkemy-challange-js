22fbca77e3f3733c5220b33d97c46d5681715506
Jon_menez <joanf.gimenez@gmail.com>
2022-07-22 10:46:29 -0300

    [ADD] verify record ID

--- backend/middlewares/validateID.js
+++ backend/middlewares/validateID.js
@@ -1,6 +1,7 @@
+const Record = require('../models/record');
 const User = require('../models/user');
 
-const validID = async (req, res, next) => {
+const validUserID = async (req, res, next) => {
     const { id } = req.params;
 
     const user = await User.findByPk(id);
@@ -11,7 +12,36 @@ const validID = async (req, res, next) => {
         });
     }
 
+    if (!user.status) {
+        return res.status(400).json({
+            msg: 'User is not active'
+        });
+    }
+
     next();
 }
 
-module.exports = validID;
+const validRecordID = async (req, res, next) => {
+    const { id } = req.params;
+
+    const record = await Record.findByPk(id);
+
+    if (!record) {
+        return res.status(404).json({
+            msg: 'Record not found'
+        });
+    }
+
+    if (!record.status) {
+        return res.status(400).json({
+            msg: 'Record is not active'
+        });
+    }
+
+    next();
+}
+
+module.exports = {
+    validUserID,
+    validRecordID
+};
