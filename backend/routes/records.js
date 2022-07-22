6f081406b4f5cb67b74065fcc3595c1c56c4b843
Jon_menez <joanf.gimenez@gmail.com>
2022-07-22 10:36:06 -0300

    [ADD] validations

--- backend/routes/records.js
+++ backend/routes/records.js
@@ -1,17 +1,39 @@
 const { Router } = require('express');
+const { check } = require('express-validator');
 const { getRecords, getRecordById, createRecord, updateRecord, deleteRecord } = require('../controllers/records');
+const { validRecordID, validateInputs, validJWT } = require('../middlewares');
 
 const router = Router();
 
 router.get('/', getRecords);
 
-router.get('/:id', getRecordById);
+router.get('/:id', [
+    validRecordID,
+], getRecordById);
 
-router.post('/', createRecord);
+router.post('/', [
+    validJWT,
+    check('description', 'Description is required').not().isEmpty(),
+    check('amount', 'Amount is required').not().isEmpty(),
+    check('date', 'Date is required').not().isEmpty(),
+    check('category', 'Category is required').not().isEmpty(),
+    check('is_income', 'Is income is required').not().isEmpty(),
+    validateInputs
+], createRecord);
 
-router.put('/:id', updateRecord);
+router.put('/:id', [
+    validJWT,
+    check('description', 'Description is required').not().isEmpty(),
+    check('amount', 'Amount is required').not().isEmpty(),
+    check('date', 'Date is required').not().isEmpty(),
+    check('category', 'Category is required').not().isEmpty(),
+    validateInputs
+], updateRecord);
 
-router.delete('/:id', deleteRecord);
+router.delete('/:id', [
+    validJWT,
+    validRecordID,
+], deleteRecord);
 
 
 module.exports = router;
