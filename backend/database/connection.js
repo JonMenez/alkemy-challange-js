df493dacab2c1bdae091f8262e4711e288cc3368
Jon_menez <joanf.gimenez@gmail.com>
2022-07-28 19:39:02 -0300

    [UPDATE] database

--- backend/database/connection.js
+++ backend/database/connection.js
@@ -1,12 +1,5 @@
 const { Sequelize } = require('sequelize');
 
-const database = new Sequelize(
-    'monigerDB',
-    'postgres',
-    process.env.DB_PASSWORD,
-    {
-        host: 'localhost',
-        dialect: 'postgres',
-    });
+const database = new Sequelize('postgresql://postgres:ofMlz61IixIlukpT9pXK@containers-us-west-26.railway.app:5697/railway');
 
 module.exports = database;
