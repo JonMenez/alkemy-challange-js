f1d64786a582bd215c0c2b3eb67e17d6a5abb3f9
Jon_menez <joanf.gimenez@gmail.com>
2022-07-29 12:14:59 -0300

    [ADD] handleClick to navigate

--- frontend/src/components/ArrowBack.jsx
+++ frontend/src/components/ArrowBack.jsx
@@ -1,9 +1,14 @@
+import { useNavigate } from 'react-router-dom'
 import arrowBack from '@icons/arrow-back.svg'
 import '@styles/arrowBack.scss'
 
 const ArrowBack = () => {
+    const navigate = useNavigate()
+
+    const handleClick = () => navigate('/')
+
     return (
-        <div className="arrow-back">
+        <div onClick={handleClick} className="arrow-back">
             <img src={arrowBack} alt="arrow back icon" />
         </div>
     )
