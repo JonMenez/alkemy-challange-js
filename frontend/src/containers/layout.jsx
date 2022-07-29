6a7518213baeee4da7a1f725421b1b6d6083fc20
Jon_menez <joanf.gimenez@gmail.com>
2022-07-29 12:15:33 -0300

    [ADD] navigate component

--- frontend/src/containers/layout.jsx
+++ frontend/src/containers/layout.jsx
@@ -1,7 +1,14 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, Navigate } from 'react-router-dom'
 import Header from '@containers/Header'
 
+
 const Layout = () => {
+
+
+    // if (!user) {
+    //     return <Navigate to='/' replace={true} />
+    // }
+
     return (
         <>
             <Header />
