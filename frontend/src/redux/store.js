0b0d6c755eacf40b3951ccbcc3e0bf90913c3107
Jon_menez <joanf.gimenez@gmail.com>
2022-07-29 12:18:12 -0300

    [ADD] records api RTK

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { recordsApi } from './apis/recordsApi'
 import userReducer from './slices/userSlice'
 
 export default configureStore({
     reducer: {
-        user: userReducer
-    }
+        user: userReducer,
+        [recordsApi.reducerPath]: recordsApi.reducer
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware()
+        .concat(recordsApi.middleware)
 })
